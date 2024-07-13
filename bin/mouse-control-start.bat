baade07bad8c2ad2db75ca2bec6477d5bb7c42b8
engineerping <47186985+engineerping@users.noreply.github.com>
2024-07-13 11:37:51 +0800

    Update and rename start.bat to mouse-control-start.bat
    update time interval

---
+++ bin/mouse-control-start.bat
@@ -0,0 +1,5 @@
+@echo off
+#java -classpath ../target/mouse-control-1.0.0.jar org.example.MouseShake
+java -classpath ../target/mouse-control-1.0.0.jar org.example.MouseShake 10000
+# or below command, which means interval is 3 seconds, default value is 180000 (3 minutes).
+# java -classpath ../target/mouse-control-1.0.0.jar org.example.MouseShake 3000
--- bin/start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-java -classpath ../target/mouse-control-1.0.0.jar org.example.MouseShake
-
-# or below command, which means interval is 3 seconds, default value is 180000 (3 minutes).
-# java -classpath ../target/mouse-control-1.0.0.jar org.example.MouseShake 3000
